c99c962f4fe56d50ca92263e0f0fc25e1535ea85
Everton Carvalho <everton.carvalho.dc@gmail.com>
2023-01-31 12:51:59 -0300

    configuring structure

---
+++ prisma/migrations/20230131150204_create_table_news/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "news" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "subtitle" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(3) DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "news_pkey" PRIMARY KEY ("id")
+);
