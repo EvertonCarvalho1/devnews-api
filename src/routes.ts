9c19f6a68c7d11ea4d360bd66374a19ed9ac79a7
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-02-01 12:02:21 -0300

    deleting news

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { CreateNewsController } from './controllers/news/CreateNewsController';
 import { ListNewsByIdController } from './controllers/news/ListNewsByIdController';
 import { ListAllNewsController } from './controllers/news/ListAllNewsController';
 import { UpdateNewsController } from './controllers/news/UpdateNewsController';
+import { DeleteNewsController } from './controllers/news/DeleteNewsController';
 
 const router = Router();
 
@@ -11,6 +12,7 @@ const createNewsController = new CreateNewsController();
 const listAllNewsController = new ListAllNewsController();
 const listNewsByIdController = new ListNewsByIdController();
 const updateNewsController = new UpdateNewsController();
+const deleteNewsController = new DeleteNewsController();
 
 router.post('/news/create', createNewsController.handle);
 
@@ -19,6 +21,8 @@ router.get('/news', listAllNewsController.handle);
 router.get('/news/details', listNewsByIdController.handle);
 
 router.put('/news/update', updateNewsController.handle);
+
+router.delete('/news/delete', deleteNewsController.handle);
 
 
 export { router };
