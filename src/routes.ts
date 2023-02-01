54e9e17c1c9f9dc810d3ed6b176780de058ac43c
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-02-01 11:07:54 -0300

    listing news details

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 
 import { CreateNewsController } from './controllers/news/CreateNewsController';
+import { ListNewsByIdController } from './controllers/news/ListNewsByIdController';
 
 
 const router = Router();
 
 const createNewsController = new CreateNewsController();
+const listNewsByIdController = new ListNewsByIdController();
 
-router.post('/news', createNewsController.handle)
+router.post('/news', createNewsController.handle);
+
+router.get('/news/details', listNewsByIdController.handle);
 
 export { router };
