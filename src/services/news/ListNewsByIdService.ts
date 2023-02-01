c014366dfc404cebd0c89b5ba2c7a478fdc130cd
Everton Carvalho <82480230+EvertonCarvalho1@users.noreply.github.com>
2023-02-01 12:13:16 -0300

    deleting news

--- src/services/news/ListNewsByIdService.ts
+++ src/services/news/ListNewsByIdService.ts
@@ -6,11 +6,16 @@ interface NewsRequest {
 
 class ListNewsByIdService {
     async execute({ news_id }: NewsRequest) {
+
         const findById = await prismaClient.news.findUnique({
             where: {
                 id: news_id
             }
         });
+
+        if (!findById) {
+            throw new Error("This news does not exist!");
+        }
 
         return findById;
     }
